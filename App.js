0c64390b4f761a39f4796d3b826f3ed4b815a0cd
Salman Saleem <salmansaleem24689@gmail.com>
2021-02-06 23:19:15 +0500

    Blood APP

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import CreateAppContainer from "./src/route/index";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./src/store/index";
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Loading } from "./src/screens.js/Loading/Index";
+// import AuthProvider from "./src/contexts/AuthContexts";
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#788eec',
+    accent: '#f1c40f',
+    background: '#fff',
+  }
+};
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ 
+    return (
+      <Provider store={store}>
+        <PersistGate loading={<Loading />} persistor={persistor}>
+        <CreateAppContainer/>
+        
+      </PersistGate>
+      </Provider>
+   
+    );
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
